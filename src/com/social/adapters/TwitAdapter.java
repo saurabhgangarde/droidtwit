e3142d29cefd53dec90569e53f1c611800dccabb
rohitsghatol <rohitsghatol@gmail.com>
2011-06-09 20:26:16 +0000

    Added following copy right to all java files
    /**
     * Copyright 2011 Saurabh Gangard & Rohit Ghatol (http://code.google.com/p/droidtwit/)
     * 
     * Licensed under the Apache License, Version 2.0 (the "License");
     * you may not use this file except in compliance with the License.
     * 
     * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
     * 
     * Unless required by applicable law or agreed to in writing, software 
     * distributed under the License is distributed on an "AS IS" BASIS, 
     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
     * See the License for the specific language governing permissions and limitations under the License.
     */

--- src/com/social/adapters/TwitAdapter.java
+++ src/com/social/adapters/TwitAdapter.java
@@ -1,14 +1,16 @@
 /**
- * Copyright Quickoffice, Inc, 2005-2009
+ * Copyright 2011 Saurabh Gangard & Rohit Ghatol (http://code.google.com/p/droidtwit/)
  * 
- * NOTICE: The intellectual and technical concepts contained herein are proprietary to Quickoffice, Inc. and is
- * protected by trade secret and copyright law. Dissemination of any of this information or reproduction of this
- * material is strictly forbidden unless prior written permission is obtained from Quickoffice, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * 
- * Created: Jun 3, 2011 Author: (sg)
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License.
  */
-
 package com.social.adapters;
 
 import java.util.List;
