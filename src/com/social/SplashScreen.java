ede57100e64089a0cf5f162aea1c486ca5408c07
saurabhgangarde <saurabhgangarde@gmail.com>
2011-06-10 09:03:18 +0000

    Adding Settings for Refresh Interval

--- src/com/social/SplashScreen.java
+++ src/com/social/SplashScreen.java
@@ -1,15 +1,14 @@
 /**
  * Copyright 2011 Saurabh Gangarde & Rohit Ghatol (http://code.google.com/p/droidtwit/)
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License.
  * 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.social;
 
@@ -28,15 +27,16 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
+import com.social.services.managers.FeedManager;
 import com.social.services.managers.OAuthAuthenticatonMgr;
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends Activity
+{
 
 	private static final int TWITTER = 1;
 	private static final String TWITTER_KEY = "QFgKeMtBipewO4IG0rCNvw";
 	private static final String TWITTER_SECRET = "OLUqNsO5oSRrv8MgjAHZ0zgHx60tHzfQ7P3dbzzZPoI";
 	private static final String CALLBACK_URL = "DroidTwit://twitt";
-	
 
 	private OAuthSignpostClient client;
 	private ImageButton twitterButton;
@@ -45,133 +45,112 @@ public class SplashScreen extends Activity {
 	private String verifier;
 	private String[] accessTokenAndSecret;
 
+
 	/**
-	 * Called when the activity is first created. Here we will setup oAuth
-	 * related implementations.
+	 * Called when the activity is first created. Here we will setup oAuth related implementations.
 	 * 
 	 * */
 
-	public void onCreate(final Bundle savedInstanceState) {
+	@Override
+	public void onCreate(final Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		
+
 		setAlarm();
 		setContentView(R.layout.main);
 		authMgr = new OAuthAuthenticatonMgr(getApplicationContext());
-		twitterButton = (ImageButton) findViewById(R.id.twitter);
+		twitterButton = (ImageButton)findViewById(R.id.twitter);
 
-		if (authMgr.isAuthenticationRequired()) {
+		if ( authMgr.isAuthenticationRequired() )
+		{
 			twitterButton.setVisibility(View.VISIBLE);
 			createAuthorizationRequests(twitterButton);
-		} else {
+		}
+		else
+		{
 			twitterButton.setVisibility(View.INVISIBLE);
 			navigateToSocialFeed();
 		}
 	}
 
-	private void setAlarm(){
-		AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 100,
-				intent, PendingIntent.FLAG_UPDATE_CURRENT);
-		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (5000), 60000,pendingIntent);
 
-	}
-	protected void onResume() {
-		super.onResume();
-		Log.d("DroidTwit", "ON RESUME");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onPause()
+	/**
+	 * Set alarm manager for auto-refreshing twits
 	 */
+	private void setAlarm()
+	{
+		final AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+		final Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
+		final PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 100, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		final FeedManager feedManager = new FeedManager(getApplicationContext());
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (5000), feedManager
+				.getTwitterFeedRefreshInterval(), pendingIntent);
 
-	protected void onPause() {
-		super.onPause();
-		Log.d("DroidTwit", "ON PAUSE");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onStart()
-	 */
-
-	protected void onStart() {
-		super.onStart();
-		Log.d("DroidTwit", "ON START");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onDestroy()
-	 */
-
-	protected void onDestroy() {
-		super.onDestroy();
-		Log.e("DroidTwit", "ON DESTROY");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onStop()
-	 */
-
-	protected void onStop() {
-		super.onStop();
-		Log.e("DroidTwit", "ON STOP");
-	}
 
 	/**
 	 * Create authorization request and launch login url in the browser
 	 * 
 	 * @param button
 	 */
-	void createAuthorizationRequests(final ImageButton button) {
+	void createAuthorizationRequests(final ImageButton button)
+	{
 		// Set on click listener
 		button.setOnClickListener(new OnClickListener() {
 
-			public void onClick(final View view) {
+			public void onClick(final View view)
+			{
 				String authUrl = null;
-				try {
+				try
+				{
 					authUrl = getAuthorizationUrl(TWITTER);
 
-					startActivity(new Intent(Intent.ACTION_VIEW, Uri
-							.parse(authUrl)));
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
 
-				} catch (final Exception e) {
-					Log.d("DroidTwit",
-							"Caught exception in createAuthorizationRequests "
-									+ e.getMessage());
+				}
+				catch ( final Exception e )
+				{
+					Log.d("DroidTwit", "Caught exception in createAuthorizationRequests " + e.getMessage());
 				}
 			}
 		});
 	}
 
+
 	/**
-	 * When we get callback from browser after authentication, get the twits and
-	 * Launch ListActivity - SocialFeed to display these twits
+	 * When we get callback from browser after authentication, get the twits and Launch ListActivity - SocialFeed to
+	 * display these twits
 	 */
-	protected void onNewIntent(final Intent intent) {
+	@Override
+	protected void onNewIntent(final Intent intent)
+	{
 		super.onNewIntent(intent);
-		try {
+		try
+		{
 			saveAccessToken(intent);
 			navigateToSocialFeed();
-		} catch (Exception e) {
+		}
+		catch ( final Exception e )
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
-	private void navigateToSocialFeed() {
-		Intent navIntent = new Intent(getApplicationContext(), SocialFeed.class);
+
+	/**
+	 * GO to List View to load twits
+	 */
+	private void navigateToSocialFeed()
+	{
+		final Intent navIntent = new Intent(getApplicationContext(), SocialFeed.class);
 		finish();
 		startActivity(navIntent);
 	}
+
 
 	/**
 	 * Get authorization url according social service selected
@@ -180,11 +159,11 @@ public class SplashScreen extends Activity {
 	 * @return
 	 * @throws Exception
 	 */
-	private String getAuthorizationUrl(final int socialId) throws Exception {
+	private String getAuthorizationUrl(final int socialId) throws Exception
+	{
 		String authUrl = null;
 
-		client = new OAuthSignpostClient(TWITTER_KEY, TWITTER_SECRET,
-				CALLBACK_URL);
+		client = new OAuthSignpostClient(TWITTER_KEY, TWITTER_SECRET, CALLBACK_URL);
 
 		final URI twitterUrl = client.authorizeUrl();
 		authUrl = twitterUrl.toString();
@@ -193,17 +172,20 @@ public class SplashScreen extends Activity {
 		return authUrl;
 	}
 
+
 	/**
 	 * Get access token from verifier received in callback URL
 	 * 
 	 * @param intent
 	 * @throws Exception
 	 */
-	private void saveAccessToken(final Intent intent) throws Exception {
+	private void saveAccessToken(final Intent intent) throws Exception
+	{
 
 		Log.e("OnResume", "Fetching access token ...");
 		final Uri uri = intent.getData();
-		if ((uri != null) && uri.toString().startsWith(CALLBACK_URL)) {
+		if ( (uri != null) && uri.toString().startsWith(CALLBACK_URL) )
+		{
 			verifier = uri.getQueryParameter("oauth_verifier");
 			Log.e("OnResume", verifier);
 
@@ -214,9 +196,68 @@ public class SplashScreen extends Activity {
 			Log.e("NewIntent", "Access token: " + accessTokenAndSecret[0]);
 			Log.e("NewIntent", "Token secret: " + accessTokenAndSecret[1]);
 
-			authMgr.saveAuthTokens(accessTokenAndSecret[0],
-					accessTokenAndSecret[1]);
+			authMgr.saveAuthTokens(accessTokenAndSecret[0], accessTokenAndSecret[1]);
 		}
 
+	}
+
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		Log.d("DroidTwit", "ON RESUME");
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		Log.d("DroidTwit", "ON PAUSE");
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onStart()
+	 */
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		Log.d("DroidTwit", "ON START");
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		Log.e("DroidTwit", "ON DESTROY");
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onStop()
+	 */
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		Log.e("DroidTwit", "ON STOP");
 	}
 }
--- src/com/social/services/SocialServiceImpl.java
+++ src/com/social/services/SocialServiceImpl.java
@@ -1,15 +1,14 @@
 /**
  * Copyright 2011 Saurabh Gangarde & Rohit Ghatol (http://code.google.com/p/droidtwit/)
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License.
  * 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.social.services;
 
@@ -35,37 +34,40 @@ import com.social.model.Twit;
 import com.social.services.managers.FeedManager;
 import com.social.services.managers.OAuthAuthenticatonMgr;
 
-public class SocialServiceImpl extends Service {
+public class SocialServiceImpl extends Service
+{
 	private static final String TAG = "SocialService";
 	// FIXME - Need more optimum logic, but this will do for the demo
 	private final ISocialService.Stub mBinder = new ISocialService.Stub() {
 
 		// FIXME - Need more optimum logic, but this will do for the demo
-		public List<Twit> getSocialFeed() throws RemoteException {
+		public List<Twit> getSocialFeed() throws RemoteException
+		{
 			return getDBTwits();
 
 		}
 
+
 		// FIXME - Need more optimum logic, but this will do for the demo
-		public List<Twit> getCurrentSocialFeed() throws RemoteException {
-			FeedManager feedManager = new FeedManager();
-			OAuthAuthenticatonMgr authMgr = new OAuthAuthenticatonMgr(
-					getApplicationContext());
-			if (!authMgr.isAuthenticationRequired()) {
-				List<Twit> twits = feedManager.getSocialFeed(authMgr
-						.getAuthTokens());
-				DBAdapter dbAdapter = new DBAdapter(getApplicationContext());
+		public List<Twit> getCurrentSocialFeed() throws RemoteException
+		{
+			final FeedManager feedManager = new FeedManager(getApplicationContext());
+			final OAuthAuthenticatonMgr authMgr = new OAuthAuthenticatonMgr(getApplicationContext());
+			if ( !authMgr.isAuthenticationRequired() )
+			{
+				final List<Twit> twits = feedManager.getSocialFeed(authMgr.getAuthTokens());
+				final DBAdapter dbAdapter = new DBAdapter(getApplicationContext());
 				dbAdapter.open();
-				for (Twit twit : twits) {
-					long noOfRowsEffected = dbAdapter.updateTwit(
-							twit.getTwitId(), twit.getProfileName(),
-							twit.getImageUrl(), twit.getTwitMessage());
+				for ( final Twit twit : twits )
+				{
+					final long noOfRowsEffected = dbAdapter.updateTwit(twit.getTwitId(), twit.getProfileName(), twit
+							.getImageUrl(), twit.getTwitMessage());
 					// Check for new twit
-					if (noOfRowsEffected < 1) {
+					if ( noOfRowsEffected < 1 )
+					{
 						// Insert if not already present
-						dbAdapter.insertTwit(twit.getTwitId(),
-								twit.getProfileName(), twit.getImageUrl(),
-								twit.getTwitMessage());
+						dbAdapter.insertTwit(twit.getTwitId(), twit.getProfileName(), twit.getImageUrl(), twit
+								.getTwitMessage());
 
 					}
 
@@ -73,32 +75,39 @@ public class SocialServiceImpl extends Service {
 				dbAdapter.close();
 
 				return getDBTwits();
-			} else {
+			}
+			else
+			{
 				return new ArrayList<Twit>();
 			}
 		}
+
 
 		/**
 		 * 
 		 * @return All Twits present in the DB
 		 */
-		private List<Twit> getDBTwits() {
+		private List<Twit> getDBTwits()
+		{
 
-			DBAdapter dbAdapter = new DBAdapter(getApplicationContext());
+			final DBAdapter dbAdapter = new DBAdapter(getApplicationContext());
 			dbAdapter.open();
-			Cursor cursor = dbAdapter.getAllTwits();
-			try {
-				int dbEntries = cursor.getCount();
-				List<Twit> dbTwits = new ArrayList<Twit>();
-				while (cursor.moveToNext()) {
-					dbTwits.add(new Twit(cursor.getLong(0),
-							cursor.getString(1), cursor.getString(2), cursor
-									.getString(3)));
+			final Cursor cursor = dbAdapter.getAllTwits();
+			try
+			{
+				final int dbEntries = cursor.getCount();
+				final List<Twit> dbTwits = new ArrayList<Twit>();
+				while ( cursor.moveToNext() )
+				{
+					dbTwits.add(new Twit(cursor.getLong(0), cursor.getString(1), cursor.getString(2), cursor.getString(3)));
 				}
 
 				return dbTwits;
-			} finally {
-				if (null != cursor) {
+			}
+			finally
+			{
+				if ( null != cursor )
+				{
 					cursor.close();
 				}
 				dbAdapter.close();
@@ -107,34 +116,44 @@ public class SocialServiceImpl extends Service {
 
 	};
 
+
 	@Override
-	public IBinder onBind(final Intent intent) {
+	public IBinder onBind(final Intent intent)
+	{
 		return mBinder;
 	}
 
+
 	@Override
-	public void onCreate() {
+	public void onCreate()
+	{
 		Log.d(TAG, "onCreate");
 
 	}
 
+
 	@Override
-	public void onDestroy() {
+	public void onDestroy()
+	{
 		Log.d(TAG, "onDestroy");
 		// [timer release] Thank god, its not Objective-C
 	}
 
+
 	@Override
-	public void onStart(final Intent intent, final int startid) {
+	public void onStart(final Intent intent, final int startid)
+	{
 		Log.d(TAG, "onStart");
 
-		if (null != intent
-				&& intent.getExtras().containsKey("ACTION")
-				&& "UPDATE_FEEDS"
-						.equals(intent.getExtras().getString("ACTION"))) {
-			try {
+		if ( (null != intent) && intent.getExtras().containsKey("ACTION")
+				&& "UPDATE_FEEDS".equals(intent.getExtras().getString("ACTION")) )
+		{
+			try
+			{
 				updateFeeds();
-			} catch (RemoteException e) {
+			}
+			catch ( final RemoteException e )
+			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -142,40 +161,43 @@ public class SocialServiceImpl extends Service {
 
 	}
 
+
 	// FIXME - Need more optimum logic, but this will do for the demo
-	private void updateFeeds() throws RemoteException {
+	private void updateFeeds() throws RemoteException
+	{
 		System.out.println("updateFeed called at " + (new Date()));
-		Runnable runnable = new Runnable() {
+		final Runnable runnable = new Runnable() {
 
 			@Override
-			public void run() {
+			public void run()
+			{
 				// TODO Auto-generated method stub
 
 				boolean sendNotification = false;
-				FeedManager feedManager = new FeedManager();
-				OAuthAuthenticatonMgr authMgr = new OAuthAuthenticatonMgr(
-						getApplicationContext());
-				if (!authMgr.isAuthenticationRequired()) {
-					List<Twit> twits = feedManager.getSocialFeed(authMgr
-							.getAuthTokens());
-					DBAdapter dbAdapter = new DBAdapter(getApplicationContext());
+				final FeedManager feedManager = new FeedManager(getApplicationContext());
+				final OAuthAuthenticatonMgr authMgr = new OAuthAuthenticatonMgr(getApplicationContext());
+				if ( !authMgr.isAuthenticationRequired() )
+				{
+					final List<Twit> twits = feedManager.getSocialFeed(authMgr.getAuthTokens());
+					final DBAdapter dbAdapter = new DBAdapter(getApplicationContext());
 					dbAdapter.open();
-					for (Twit twit : twits) {
-						long noOfRowsEffected = dbAdapter.updateTwit(
-								twit.getTwitId(), twit.getProfileName(),
-								twit.getImageUrl(), twit.getTwitMessage());
+					for ( final Twit twit : twits )
+					{
+						final long noOfRowsEffected = dbAdapter.updateTwit(twit.getTwitId(), twit.getProfileName(), twit
+								.getImageUrl(), twit.getTwitMessage());
 						// Check for new twit
-						if (noOfRowsEffected < 1) {
+						if ( noOfRowsEffected < 1 )
+						{
 							// Insert and mark for notification
-							dbAdapter.insertTwit(twit.getTwitId(),
-									twit.getProfileName(), twit.getImageUrl(),
-									twit.getTwitMessage());
+							dbAdapter.insertTwit(twit.getTwitId(), twit.getProfileName(), twit.getImageUrl(), twit
+									.getTwitMessage());
 							// Notify so that user comes to know about this
 							sendNotification = true;
 						}
 
 					}
-					if (sendNotification) {
+					if ( sendNotification )
+					{
 						sendNotification();
 					}
 
@@ -185,32 +207,30 @@ public class SocialServiceImpl extends Service {
 			}
 
 		};
-		Thread thread = new Thread(runnable);
+		final Thread thread = new Thread(runnable);
 		thread.start();
 
 	}
 
-	private void sendNotification() {
+
+	private void sendNotification()
+	{
 		final String ns = Context.NOTIFICATION_SERVICE;
-		final NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+		final NotificationManager mNotificationManager = (NotificationManager)getSystemService(ns);
 		final int icon = R.drawable.twitter_icon;
 		final CharSequence tickerText = "Twitter";
 		final long when = System.currentTimeMillis();
 
-		final Notification notification = new Notification(icon, tickerText,
-				when);
+		final Notification notification = new Notification(icon, tickerText, when);
 
 		final Context context = getApplicationContext();
 		final CharSequence contentTitle = "New Twits";
 		final CharSequence contentText = "You have new twits!";
-		final Intent notificationIntent = new Intent(getApplicationContext(),
-				SocialFeed.class);
+		final Intent notificationIntent = new Intent(getApplicationContext(), SocialFeed.class);
 
-		final PendingIntent contentIntent = PendingIntent.getActivity(
-				getApplicationContext(), 0, notificationIntent, 0);
+		final PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, 0);
 
-		notification.setLatestEventInfo(context, contentTitle, contentText,
-				contentIntent);
+		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
 
 		final int HELLO_ID = 1;
 
